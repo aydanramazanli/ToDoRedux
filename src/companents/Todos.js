d84714519160d5704d3e1c0a0955df3ba1f56313
Aydan <aydan2903@gmail.com>
2022-03-25 16:04:32 +0400

    completed added

--- src/companents/Todos.js
+++ src/companents/Todos.js
@@ -1,62 +1,65 @@
-import React, {useState} from 'react'
-import {connect} from 'react-redux'
-import {addToDo, removeTodo} from '../redux/reducer'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addToDo, removeTodo, updateTodo , completeTodo} from "../redux/reducer";
+import ToDoItems from "./ToDoItems";
 
-const mapStateToProps =(state) =>{
-    return {
-        todo:state  //birinci hisse variable secirsen
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    todo: state, //birinci hisse variable secirsen
+  };
+};
 
 // from reducer
-const mapDispatchToProps=(dispatch) =>{
-    return {
-        addToDos:(obj)=>dispatch(addToDo(obj)),
-        removeTodos:(id)=>dispatch(removeTodo(id))
-    }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToDos: (obj) => dispatch(addToDo(obj)),
+    removeTodos: (id) => dispatch(removeTodo(id)),
+    updateTodos: (obj) => dispatch(updateTodo(obj)),
+    completeTodos: (id) => dispatch(completeTodo(id)),
+  };
+};
 
-}
 const Todos = (props) => {
-    const [todo, setTodo] =useState("")
+  const [todo, setTodo] = useState("");
 
-    const inputValue =(e)=>{
-        setTodo(e.target.value)
-    }
 
-    console.log(props)
+  const inputValue = (e) => {
+    setTodo(e.target.value);
+  };
+ 
+  
+
+
   return (
+    <div className="Todos">
+      <input
+        type="text"
+        onChange={(e) => inputValue(e)}
+        className="todo-input"
+      />
+      {/* add list */}
+      <button
+        onClick={() =>
+          props.addToDos({
+            id: Math.floor(Math.random() * 1000),
+            item: todo,
+            completed: false,
+          })
+        }
+      >
+        Add
+      </button>
 
-    <div className="Todos"
-    >
-        <input type='text' onChange={(e)=>inputValue(e)} className="todo-input"/>
-        {/* add list */}
-        <button onClick={()=>props.addToDos({
-              id:Math.floor(Math.random()*1000)  ,
-              item:todo,
-              completed:false,
-        })}>
-            Add
-        </button>
-
-
-<br/>
-        <ul>
-            {
-            props.todo.map((elem)=>{
-                return(
-                    <li key={elem.id}>
-                        {elem.item}
-                        <button
-                        onClick={()=>{props.removeTodos(elem.id)}}
-                        >Delete</button>
-                    </li>
-                )
-            })
-            }
-        </ul>
-
+      <br />
+      {/* <ul>
+        {props.todo.map((elem) => {
+          return (
+          <ToDoItems />
+          );
+        })}
+      </ul> */}
     </div>
-  )
-}
+  );
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(Todos)
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -17,8 +17,35 @@ const toDoReducer=createSlice({
         removeTodo: (state, action) => {
             return state.filter((item)=> item.id !== action.payload)
         },
+
+        //update list
+        updateTodo: (state, action) => {
+            return state.filter((item)=>{
+                if(item.id === action.payload.id){
+                    return{
+                        ...item,
+                        item:action.payload.item
+                    }
+                    
+                }
+                return item;
+            })
+        },
+        //completed list
+        completeTodo: (state, action)=>{
+            return state.map((item)=>{
+                if(item.id === action.payload){
+                    return{
+                        ...item,
+                        completed: true,
+                    }
+                    
+                }
+                return item;
+            })
+        }
     }
 })
 
-export const {addToDo,removeTodo} =toDoReducer.actions
+export const {addToDo,removeTodo,updateTodo,completeTodo} =toDoReducer.actions
 export const reducer = toDoReducer.reducer
